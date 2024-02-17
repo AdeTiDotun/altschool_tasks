4c4af95730392db89882d85a2896444944bad3c7
AdeTiDotun <adejumodaniel274@gmail.com>
2024-02-17 02:10:54 +0100

    learning modules

---
+++ testFile/index.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    return a + b;
+}
+
+module.exports = sum;
+
+// test ("adds two numbers together", ()=> {
+//     expects(sum(1,3)).toBe(3)
+// });
+
---
+++ testFile/test.js
@@ -0,0 +1,9 @@
+// function sum(a,b) {
+//     return a + b;
+// }
+
+const sum = require("jest/package.json");
+
+test ("adds two numbers", ()=> {
+    expects(sum(1,3)).toBe(4);
+});
